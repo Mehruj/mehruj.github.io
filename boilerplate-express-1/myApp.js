b68c5d59a63f5c98eac1c31e114ae627bf1958bd
Mehruj <49351612+Mehruj@users.noreply.github.com>
2021-11-11 00:41:35 -0500

    Update myApp.js

--- boilerplate-express-1/myApp.js
+++ boilerplate-express-1/myApp.js
@@ -1,9 +1,17 @@
-require('dotenv').config();
+const mySecret = process.env['MESSAGE_STYLE'];
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 var urlPraserencode= bodyParser.urlencoded({extended: false});
 console.log('hello world');
+
+app.use(function middleware(req, res, next) {
+ 
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
+
 app.get("/", function(req, res) {
   // res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
@@ -17,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-const mysecvar = process.env['MESSAGE_STYLE'];
 app.get("/json", (req, res) => {
   if (process.env.MESSAGE_STYLE === "uppercase") {
     response = "HELLO JSON";
@@ -28,19 +35,10 @@ app.get("/json", (req, res) => {
     "message": response
   });
 });
+app.use(express.static(__dirname + "/public"));
 
-app.use(function middleware(req, res, next) {
-   
-  console.log(req.method + " " + req.path + " - " +req.ip);
-  next();
-});
 
-app.get("/:word/echo", (req, res) => {
-  const { word } = req.params;
-  res.json({
-    echo: word
-  });
-});
+
 app.get(
   "/now",
   (req, res, next) => {
